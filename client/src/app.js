2c9af8a9db2e606551affd5506629e4601d418ec
Saad Farooq <sa@dfarooq.com>
2014-07-12 05:17:31 -0400

    Testing works

--- client/src/app.js
+++ client/src/app.js
@@ -1,13 +1,6 @@
 angular.module('irMainPage', ['irServices'])
 .controller( 'AppCtrl', function ( $scope, dataFactory ) {
-  var rows = [
-    {"name": "Saad", "value": "    ---    Aweomsenesnesss"},
-    {"name": "Saad", "value": "    ---    Awesome"},
-    {"name": "Saad", "value": "    ---    Aweomse"},
-    {"name": "Saad", "value": "    ---    Aweomse"}
-  ];
-  $scope.rows = rows;
-  dataFactory.getUsers().success(function(data) {
-    console.log(data);
-  })
+  dataFactory.getUsers().success(function(users) {
+    $scope.rows = users;
+  });
 });
--- client/src/app.spec.js
+++ client/src/app.spec.js
@@ -1,24 +1,46 @@
-describe('isCurrentUrl', function () {
-  var AppCtrl, $location, $scope;
+describe('Main page test', function() {
+  var AppCtrl, $scope;
 
-  beforeEach(module('irMainPage'));
+  beforeEach(function() {
+      module('irMainPage', 'irServices');
+  });
 
-  beforeEach(inject(function ($controller, _$location_, $rootScope) {
-    $location = _$location_;
+  beforeEach(inject(function($controller, $rootScope) {
     $scope = $rootScope.$new();
-    AppCtrl = $controller('AppCtrl', {
-      $location: $location,
-      $scope: $scope
-    });
+    var mockDataFactory = {
+        getUsers: function() {
+            return {
+                success: function(fn) {
+                    fn(
+                        [{
+                            'name': 'John',
+                            'age': 27
+                        }, {
+                            'name': 'Andrew',
+                            'age': 23
+                        }]
+
+                    );
+                }
+            };
+        }
+    };
+
+
+      AppCtrl = $controller('AppCtrl', {
+          $scope: $scope,
+          dataFactory: mockDataFactory
+      });
   }));
 
 
-  it('should pass a dummy test', inject(function () {
-    expect(AppCtrl).toBeTruthy();
+  it('should pass a dummy test', inject(function() {
+      expect(AppCtrl).toBeTruthy();
   }));
 
-  it('should list values', inject(function () {
-    expect($scope.rows.length).toBe(4);
-    expect($scope.rows[0].name).toBe('Saad');
-  }))
+  it('should list values', inject(function() {
+      expect($scope.rows.length).toBe(2);
+      expect($scope.rows[0].name).toBe('John');
+  }));
+
 });
